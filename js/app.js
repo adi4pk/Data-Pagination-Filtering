55702e08d7cdd474e6070aaca4d4ac9062e64199
Adrian Stancioiu <adrian.stancioiu96@yahoo.com>
2025-11-11 21:42:57 +0200

    4th commit - added EDIT student button - implemented addStudent button/ saveStudent button function.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ let studentContainer = document.querySelector(".student-list");
 let btnList = document.querySelector(".link-list");
 let btn = document.querySelector(".button");
 
-
 let modalContainer = document.querySelector(".modal-wrapper");
 let addStudentBtn = document.querySelector(".add-student-btn");
 let discardStd = modalContainer.querySelector(".discardStd");
@@ -20,63 +19,146 @@ let emailField = document.querySelector(".std-email");
 let joindate = document.querySelector(".std-joindate");
 
 
+
 let leftArrow = document.querySelector(".left-arrow");
 let rightArrow = document.querySelector(".right-arrow");
 
+let displayStudContainer = document.querySelector(".display-student-modal");
+let leftModalArrow = document.querySelector(".modal-arrow-left");
+let rightModalArrow = document.querySelector(".modal-arrow-right");
 
-let numberOfElementPerPage=8;
+let numberOfElementPerPage = 8;
 let currentStudentIndex = 0;
 
-attachCards(pagination(data,1,numberOfElementPerPage));
+loadFromLocalStorage();
+
+attachCards(pagination(data, 1, numberOfElementPerPage));
 attachButtons(numberOfElementPerPage);
+
+// let btnEdit = document.querySelector(".edit-btn");
 
 //event
 
-btnList.addEventListener("click",(ev) =>{
+btnList.addEventListener("click", (ev) => {
+  let obj = ev.target;
+  if (obj.classList.contains("btn-page")) {
+    console.log(obj.textContent);
+    attachCards(pagination(data, obj.textContent, numberOfElementPerPage));
+  }
+});
+
+addStudentBtn.addEventListener("click", () => {
+  modalContainer.classList.add("show");
+  modalContainer.classList.toggle("hide");
+});
+
+discardStd.addEventListener("click", () => {
+  modalContainer.classList.toggle("hide");
+});
+
+saveStud.addEventListener("click", () => {
+  //todo: functie validare inputuri
+
+  //todo: functie ce extrage datele din inputuri si returneaza un student cudatele din inputuri
+
+  ///todo: save and close
+  let studentNou = saveCard();
+  data.push(studentNou);
+  attachCards(pagination(data, 1, numberOfElementPerPage));
+  attachButtons(numberOfElementPerPage);
+  modalContainer.classList.toggle("hide");
+
+  saveToLocalStorage();
+});
+
+// leftArrow.addEventListener("click", () =>{
+//     updateModal(currentStudentIndex -1);
+// })
+
+// rightArrow.addEventListener("click", () =>{
+//     updateModal(currentStudentIndex +1);
+// })
+
+studentContainer.addEventListener("click", (ev) => {
+  //--event bubbling - addEventListener pe containerul principal
+  let obj = ev.target;
+
+  if (obj.classList.contains("email")) {
+    let student = findStudentByEmail(obj.textContent);
+
+    viewStudentModal(student);
+  }
+});
+
+
+// btnEdit.addEventListenre("click", (ev) =>{}...)          // WRONG X 
+studentContainer.addEventListener("click", (ev) => {
     let obj = ev.target;
-    if(obj.classList.contains("btn-page")){
+    if(obj.classList.contains("edit-btn")){
+        // if(!(editModal))
+    console.log("TESTETESTETETEDSTETST");
+    let card = obj.parentNode;
+    console.log(card.children);
+    let avatar = card.children[0].children[0];
 
-        console.log(obj.textContent);
-        attachCards(pagination(data,obj.textContent, numberOfElementPerPage));
+    card.innerHTML =
+    `<div class="edit-student">
+    <img class="avatar" src=${avatar.src}>
+    <div class="edit-name-section">
+      <h1 class="edit-std-first">First name</h1>
+      <input type="text" class="firstname-value">
+      <h1 class="edit-std-last">Last Name</h1>
+      <input type="text" class="lastname-value">
+    </div>
+    <p class="edit-std-email">Email:</p>
+    <input type="text" class="email-value">
+    <p class="edit-std-joindate">Joined in:</p>
+    <input type="text" class="joindate-value">
+    <div class="button-section">
+      <button class="saveStd btn">SAVE</button>
+      <button class="discardStd btn">discard</button>
+    </div>
+  </div>`;
 
+
+    // generateEditModal()
     }
 });
 
-addStudentBtn.addEventListener("click", ()=>{
-    modalContainer.classList.add("show");
-    modalContainer.classList.toggle("hide");
-})
 
-discardStd.addEventListener("click", () =>{
-    modalContainer.classList.toggle("hide");
-})
-
-
-saveStud.addEventListener("click", () =>{
-
-    modalContainer.classList.toggle("hide");
-    const newStud = saveCard();
-    data.push(newStud);
-
-    let newCard = createCard(newStud);
-    studentContainer.append(newCard);
-
-    attachCards(pagination(data,1,numberOfElementPerPage));
-    studModalContainer.classList.toggle("hide");
-
-    studentModal(newStud);
-
-    // console.log("test");
-    // console.log(data);
-
-
+studentContainer.addEventListener("click", (ev) =>{
+    let obj = ev.target;
+    // let card = obj.parentNode.parentNode.parentNode;     // pay attention to the HTML structure
+    // let card = obj.closest(".card");
+    let card = obj.parentNode;
     
-})
+    if(obj.classList.contains("saveStd")){
+        console.log(card);
 
-leftArrow.addEventListener("click", () =>{
-    updateModal(currentStudentIndex -1);
-})
+        let avatar = card.children[0].children[0];
 
-rightArrow.addEventListener("click", () =>{
-    updateModal(currentStudentIndex +1);
+        let firstName = card.querySelector(".firstname-value");
+        let lastName = card.querySelector(".lastname-value");
+        let email = card.querySelector(".email-value");
+        let joinDate = card.querySelector(".joindate-value");
+
+        card.innerHTML =
+            `<div class="edit-student">
+            <img class="avatar" src=${avatar.src}>
+            <div class="edit-name-section">
+            <h1 class="edit-std-first">${firstName.value}</h1>
+            
+            <h1 class="edit-std-last">${lastName.value}</h1>
+            
+            </div>
+            <p class="edit-std-email">${email.value}</p>
+           
+            <p class="edit-std-joindate">${joinDate.value}</p>
+            
+            <div class="button-section">
+            <button class="saveStd btn">SAVE</button>
+            <button class="discardStd btn">discard</button>
+            </div>
+        </div>`;
+    }
 })
--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-// Call functions
-
-//showPage -- append a page of nine students
-
 
 
 // functie ce creeaza un student
@@ -41,7 +16,8 @@ function createCard(student) {
             </div>
             <div class="joined-details">
                <span class="date">Joined ${student.registered.date}</span>
-            </div>`;
+            </div>
+            <button class="edit-btn">EDIT</button>`;
 
   return card;
 }
@@ -57,97 +33,269 @@ function attachCards(arr) {
     });
 }
 //           0   1  2  3  4  5  6  7  8  9  10 11 12 13
-let pages = [12,32,43,11,45,16,97,28,91,12,34,64,78,91]   //arr  pagina perPagina
+let pages = [12, 32, 43, 11, 45, 16, 97, 28, 91, 12, 34, 64, 78, 91]; //arr  pagina perPagina
 
-
-// pages  2 3        pozitiile  3 4 5 
+// pages  2 3        pozitiile  3 4 5
 // pages  3 2        pozitiile  4 5
-function pagination(arr, pagina, perPagina){
+function pagination(arr, pagina, perPagina) {
+  let paged = [];
 
-  let paged=[];
-
-  for(let i=perPagina*(pagina-1); i<pagina*perPagina&&i<arr.length; i++){
+  for (
+    let i = perPagina * (pagina - 1);
+    i < pagina * perPagina && i < arr.length;
+    i++
+  ) {
     paged.push(arr[i]);
-    
   }
   return paged;
-
 }
 
-
-function createButton(number){
+function createButton(number) {
   let btn = document.createElement("li");
   btn.innerHTML = `<button type="button" class="active btn-page">${number}</button>`;
 
   return btn;
 }
 
-function attachButtons(perPagina){
-  let btnList=document.querySelector(".link-list");
-  btnList.innerHTML="";
-  for(let i=1;i<data.length/perPagina+1;i++){
-     btnList.appendChild(createButton(i));
+function attachButtons(perPagina) {
+  let btnList = document.querySelector(".link-list");
+  btnList.innerHTML = "";
+  for (let i = 1; i < data.length / perPagina + 1; i++) {
+    btnList.appendChild(createButton(i));
   }
 }
 
-function saveCard(){
+//functie ce extrage datele din inputuri si returneaza un student cu datele din inputuri
+function saveCard() {
   // let card = document.createElement("li");
   let firstName = document.querySelector(".first-box");
   let lastName = document.querySelector(".last-box");
   let email = document.querySelector(".email-box");
+  let datejoined = document.querySelector(".calendar-box");
 
-  
-  let newStudent = {
-      name: {
-        title: "", // optional
-        first: firstName.value,
-        last: lastName.value,
-      },
-      email: email.value,
-      registered: {
-        date: "02-24-2006",
-        // age: 21
-      },
-      picture: {
-        large: "https://randomuser.me/api/portraits/lego/1.jpg",
-        medium: "",
-        thumbnail: "",
-      },
-    };
-
-
+  let studentNou = {
+    name: {
+      // title: "",
+      first: firstName.value,
+      last: lastName.value,
+    },
+    email: email.value,
+    registered: {
+      date: datejoined.value,
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/lego/1.jpg",
+      medium: "",
+      thumbnail: "",
+    },
+  };
 
   firstName.value = "";
   lastName.value = "";
-  email.value ="";
-  return newStudent;
+  email.value = "";
+  datejoined.value = "";
+  return studentNou;
 }
 
+// function newStudent(student) {
+  
+//   let newStudentModal = document.createElement("div");
+//   newStudentModal.classList.add("student-modal-container");
+//   newStudentModal.innerHTML = ` <div class="student-modal">
+//                               <div class="name-section">
+                                    
+//                                       <h1>${student.name.first}</h1>
+//                                       <h1>${student.name.last}</h1>
+//                                       <h1> TEST</h1>
+                                    
+//                                   </div>
+//                               <button class="close-modal-stud">Close</button>
+//                           </div>`;
 
-function studentModal (student){
+//   firstNameField.textContent = student.name.first;
+//   lastNameField.textContent = student.name.last;
+//   emailField.textContent = `Email: ${student.email}`;
+//   joindate.textContent = `Joined in ${student.registered.date}`;
+
+//   let closeBtn = newStudentModal.querySelector(".close-modal-stud");
+//   // document.body.append(student);
+
+//   closeBtn.addEventListener("click", () => {
+//     newStudentModal.remove();
+//     document.querySelector(".student-modal-container")?.remove();
+//   });
+
+//   document.body.appendChild(newStudentModal);
+// }
+
+function findStudentByEmail(email) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].email === email) {
+      return data[i];
+    }
+  }
+
+  return -1;
+}
+
+function findPreviousStudentByEmail(email) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].email === email) {
+      return data[i];
+    }
+  }
+
+  return -1;
+}
+
+function findNextStudentByEmail(email) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].email === email) {
+      if (i == data.length) {
+        return data[0];
+      }
+
+      return data[i + 1];
+    }
+  }
+
+  return -1;
+}
+
+function displayStudent(student) {
+  
+  // Remove any existing modal before creating a new one
+  document.querySelector(".std-display-modal-container")?.remove();
+
   let studentModal = document.createElement("div");
-  studentModal.classList.add("student-modal-container");
-  studentModal.innerHTML=` <div class="student-modal">
-                              <div class="name-section">
-                                    
-                                      <p>${student.name.first}</p>
-                                      <p>${student.name.last}</p>
-                                    
-                                  </div>
-                          </div>`
+  studentModal.classList.add("show");
+  studentModal.classList.add("std-display-modal-container");
+  studentModal.innerHTML = `<button class="modal-arrow-left modal-arrow">←</button>
+                            <div class="display-student-modal">
+                                
+                                <img class="avatar" src="${student.picture.medium}" alt="Profile Picture">
+                                <div class="name-section">
+                                      
+                                        <h1>${student.name.first}</h1>
+                                        <h1>${student.name.last}</h1>
+                                        
+                                </div>
+                                <span class="email">${student.email}</span>
+                                <div class="joined-details">
+                                  <span class="date">Joined ${student.registered.date}</span>
+                                </div>
+                                <button class="close-modal">Close</button>
+                            </div>
+                            <button class="modal-arrow-right modal-arrow">→</button>`;
 
-firstNameField.textContent=student.name.first;
-lastNameField.textContent=student.name.last;
-emailField.textContent= `Email: ${student.email}`;
-joindate.textContent= `Joined in ${student.registered.date}`;
+    // studentModal.classList.toggle("hide");
 
-document.body.append(student);
+    emailField.textContent = `Email: ${student.email}`;
+    joindate.textContent = `Joined in ${student.registered.date}`;
+
+    let closeBtn = studentModal.querySelector(".close-modal");
+    // document.body.append(student);
+
+      closeBtn.addEventListener("click", () => {
+        studentModal.remove();
+      });
+
+
+
+    let leftModalArrow = studentModal.querySelector(".modal-arrow-left");
+    leftModalArrow.addEventListener("click", () =>{
+    
+    updateModal(currentStudentIndex -1);
+    console.log("test-left");
+    });
+
+
+    let rightModalArrow = studentModal.querySelector(".modal-arrow-right");
+    rightModalArrow.addEventListener("click", () =>{
+
+    
+      updateModal(currentStudentIndex +1);
+      console.log("test-right");
+    })
+
+
+
+
+     // Append modal to page
+    let page = document.querySelector(".page");
+    page.appendChild(studentModal);
+
+
+
+
+
 }
 
+function viewStudentModal(student) {
+  console.log(student);
 
-function updateModal(index){
-  if (index >= 0 && index < data.length){
-    currentStudentIndex = index;
-    studentModal(data[index]);
+  
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].email === student.email) {
+      currentStudentIndex = i;
+      break;
+    }
+  };
+
+
+  if (student !== -1) {
+    displayStudent(student);
   }
 }
+
+
+function creationModal(index){
+  if (index >=0 && index < data.length){
+    currentStudentIndex = index;
+  }
+}
+
+
+function updateModal(index) {
+  if (index >= 0 && index < data.length) {
+    currentStudentIndex = index;
+    displayStudent(data[index]);
+  }
+}
+
+
+function generateEditModal(){
+  let editModal = document.createElement("div");
+
+  editModal.classList.add("edit-student-modal-container");
+  // editModal.classList.add("hide");
+  
+
+  editModal.innerHTML = 
+  
+`<div class="edit-student-modal">
+    <img class="avatar" src="https://randomuser.me/api/portraits/lego/1.jpg">
+    <div class="edit-name-section name-student">
+      <h1 class="edit-std-first">First name</h1>
+      <input type="text">
+      <h1 class="edit-std-last">Last Name</h1>
+      <input type="text">
+    </div>
+    <p class="edit-std-email">Email:</p>
+    <input type="text">
+    <p class="edit-std-joindate">Joined in:</p>
+    <input type="text">
+    <button class="close-btn-newstud">CLOSE</button>
+    <div class="button-section">
+      <button class="saveStd btn">SAVE</button>
+      <button class="discardStd btn">discard</button>
+    </div>
+  </div>`;
+
+  document.body.appendChild(editModal);
+}
+
+
+function saveStudent(){
+
+};
